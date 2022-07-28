38f2d58e2edd2aedf72ac7e2a2b7e43c1e5a1ce2
Maryam Moshtoofar <maryammoshtoofar@gmail.com>
2022-07-28 14:17:15 +0430

    first commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import { Form } from "./components/form";
+import { Table } from "./components/table";
+import { useState, useEffect } from "react";
+import "./App.css";
+
+function App() {
+  return <div>App</div>;
+}
+export default App;
